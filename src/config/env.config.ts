b7dc35ed1c11d2e64871e06d55e0a9004f19c0b3
Bohdan Mahometa <bogdan.magometa@gmail.com>
2025-03-18 00:51:43 +0200

    Update env.config.ts

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -2,5 +2,6 @@ export const envConfig = {
   vapi: {
     apiUrl: import.meta.env.VITE_VAPI_API_URL ?? "https://api.vapi.ai",
     token: import.meta.env.VITE_VAPI_WEB_TOKEN ?? "vapi-web-token",
+    assistantId: import.meta.env.VITE_VAPI_ASSISTANT_ID ?? "assistant-id",
   },
 };
